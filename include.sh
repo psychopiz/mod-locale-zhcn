2dc4cff7bdaee4f7f1556612a9afbb13812753ce
PsychoPiz <4604198@qq.com>
2022-11-24 15:26:55 +0800

    Update include.sh

--- include.sh
+++ include.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
-## GETS THE CURRENT MODULE ROOT DIRECTORY
-MOD_SKELETON_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/" && pwd )"
+MOD_ZHCN_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/" && pwd )"
 
-source $MOD_SKELETON_ROOT"/conf/conf.sh.dist"
+source $MOD_ZHCN_ROOT"/conf/conf.sh.dist"
 
-if [ -f $MOD_SKELETON_ROOT"/conf/conf.sh" ]; then
-    source $MOD_SKELETON_ROOT"/conf/conf.sh"
+if [ -f $MOD_ZHCN_ROOT"/conf/conf.sh" ]; then
+    source $MOD_ZHCN_ROOT"/conf/conf.sh"
 fi
